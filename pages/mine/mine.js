5f7743abf3ed28a0b465535c2ba836ff6e706d3c
combdream <combdream@outlook.com>
2022-05-10 23:36:52 +0800

    q2.3
    登录静态，

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,20 +1,28 @@
 
 Page({
-    data:{
-      hasUserInfo:false,
-      userInfo:{}
+    data:{ 
+        hasUserInfo:false, 
+        userInfo:{} 
+      }, 
+      getUserProfile:function(){ 
+        wx.getUserProfile({ 
+          desc: '获取用户信息', 
+          success:(res) => { 
+            console.log(res.userInfo); 
+            this.setData({ 
+              hasUserInfo:true, 
+              userInfo:res.userInfo 
+            }) 
+          } 
+        }) 
     },
-    getUserProfile:function(){
-      wx.getUserProfile({
-        desc: '获取用户信息',
-        success:(res) => {
-          console.log(res.userInfo);
-          this.setData({
-            hasUserInfo:true,
-            userInfo:res.userInfo
-          })
-        }
-      })
-    }
+     // 退出登录
+  loginOut() {
+    this.setData({
+      userInfo: ''
+    }),
+    // 把user缓存存储为空
+    wx.setStorageSync('user', '')
+  }
   })
   
