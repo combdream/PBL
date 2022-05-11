6641aa3aa0b981494fe64eb2c453210fe9675126
combdream <combdream@outlook.com>
2022-05-11 22:58:32 +0800

    q2.3
    订单和我的登录

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -18,11 +18,55 @@ Page({
     },
      // 退出登录
   loginOut() {
-    this.setData({
-      userInfo: ''
-    }),
-    // 把user缓存存储为空
-    wx.setStorageSync('user', '')
-  }
+    wx.showModal({
+        title: '提示',
+        content: '您确定要退出登录吗',
+        success: function (res) {
+          if (res.confirm) {//这里是点击了确定以后
+            console.log('用户点击确定')
+            wx.setStorageSync('res', '');//将token置空
+            wx.redirectTo({
+              url: '/pages/login/index',//跳去登录页
+            })
+          } else {//这里是点击了取消以后
+            console.log('用户点击取消')
+          }
+        }
+      })
+  },
+  onLaunch:function(){
+      wx.login({
+        success:function(res){
+            if(res.code){
+                console.log(res.code)
+            }else{
+                console.log('获取用户登录状态失败'+res.errMsg)
+            }
+        }
+      })
+  },
+getPhoneNumber:function(e){
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+    if(e.detail.errMsg == 'getPhoneNumber:fail user deny'){
+        wx.showModal({
+            title:'提示',
+            showCancel:false,
+            content:'未授权',
+            success:function(res){}
+        })
+    }else{
+        wx.showModal({
+            title:'提示',
+            showCancel:false,
+            content:'同意授权',
+            success:function(res){}
+        })
+    }
+},
+exchangeUser:function(e){
+    
+}
   })
   
