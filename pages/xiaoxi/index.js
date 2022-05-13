ab3e1710090ec3c23639e9480980bf7a865d10e8
unknown <unknown@example.com>
2022-05-13 10:18:02 +0800

    订单详情和聊天

--- pages/xiaoxi/index.js
+++ pages/xiaoxi/index.js
@@ -1,66 +1,135 @@
-// pages/xiaoxi/index.js
+const app = getApp();
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+/**
+ * 初始化数据
+ */
+function initData(that) {
+  inputVal = '';
+
+  msgList = [{
+      speaker: 'server',
+      contentType: 'text',
+      content: '欢迎来到明日之星！'
+    },
+    {
+      speaker: 'customer',
+      contentType: 'text',
+      content: '你好'
+    }
+  ]
+  that.setData({
+    msgList,
+    inputVal
+  })
+}
+
+/**
+ * 计算msg总高度
+ */
+// function calScrollHeight(that, keyHeight) {
+//   var query = wx.createSelectorQuery();
+//   query.select('.scrollMsg').boundingClientRect(function(rect) {
+//   }).exec();
+// }
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    scrollHeight: '100vh',
+    inputBottom: 0
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    initData(this);
+    this.setData({
+      
+    });
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 获取聚焦
+   */
+  focus: function(e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
+    })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  //失去聚焦(软键盘消失)
+  blur: function(e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 0
+    })
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
+    })
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  /**
+   * 发送点击监听
+   */
+  sendClick: function(e) {
+    msgList.push({
+      speaker: 'customer',
+      contentType: 'text',
+      content: e.detail.value
+    })
+    inputVal = '';
+    this.setData({
+      msgList,
+      inputVal
+    });
 
-    },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+  },
 
-    },
+  /**
+   * 退回上一页
+   */
+  toBackClick: function() {
+    wx.navigateBack({})
+  }
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
